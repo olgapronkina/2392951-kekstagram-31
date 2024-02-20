d1420c2dc5e7553802904971cda7dc356dcf5c45
Olga Pronkina <pronolga04@gmail.com>
2024-02-20 23:44:52 +0300

    Функция для проверки, является ли строка палиндромом

--- js/functions.js
+++ js/functions.js
@@ -10,9 +10,30 @@ const controlStringLength = (string, maxLength) => {
   }
   return false;
 };
-controlStringLength();
+controlStringLength('проверяемая строка', 20);
 
 // праверяет строку с максимальной длиной 20
 // console.log(controlStringLength('проверяемая строка', 20));
 // console.log(controlStringLength('проверяемая строка', 18));
 // console.log(controlStringLength('проверяемая строка', 10));
+
+/*Функция для проверки, является ли строка палиндромом.
+Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.*/
+
+const definePolindrom = (string) => {
+  const withoutSpacesString = string.replaceAll(' ', '');
+  const lowerCaseString = lowerCaseString.toLowerCase();
+
+  let newString = '';
+  for (index = lowerCaseString.length - 1; index >= 0; index--) {
+    newString += lowerCaseString[index];
+  }
+  if (newString === lowerCaseString) {
+    return true;
+  }
+  return false;
+};
+
+// console.log(definePolindrom('топот'));
+// console.log(definePolindrom('ДовОд'));
+// console.log(definePolindrom('Кекс'));
