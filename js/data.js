f59a43ebec38b2ea36f0fadd3af175a8e7f295ef
Olga Pronkina <pronolga04@gmail.com>
2024-03-30 15:11:53 +0300

    добавляет модуль с денными

---
+++ js/data.js
@@ -0,0 +1,69 @@
+const POST_DESCRIPTIONS = [
+  'Какая красота!',
+  'Be happy',
+  'Сыыыырр',
+  'This is my outfit for today',
+  'С семьёй',
+  'Отпуск!!!',
+];
+
+const COMMENT_NAMES = [
+  'Кира',
+  'Платон',
+  'Артём',
+  'Глеб',
+  'Максим',
+  'Алёна',
+  'Таисия',
+  'Тимофей',
+  'Анастасия',
+  'Андрей ',
+];
+
+const COMMENT_MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const POST_COUNT = 25;
+
+const RangeIDs = {
+  MIN: 1,
+  MAX: 25,
+};
+
+const RangeIDComment = {
+  MIN: 1,
+  MAX: 1000,
+};
+
+const RangeURLs = {
+  MIN: 1,
+  MAX: 25,
+};
+
+const RangeLikes = {
+  MIN: 15,
+  MAX: 200,
+};
+
+const countOfComments = {
+  MIN: 0,
+  MAX: 30,
+};
+
+export {
+  POST_DESCRIPTIONS,
+  COMMENT_NAMES,
+  COMMENT_MESSAGES,
+  POST_COUNT,
+  RangeIDs,
+  RangeIDComment,
+  RangeURLs,
+  RangeLikes,
+  countOfComments,
+};
