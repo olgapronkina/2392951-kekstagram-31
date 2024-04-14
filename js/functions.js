5248821885df403738889989270dfb9983508bc2
Olga Pronkina <pronolga04@gmail.com>
2024-04-14 19:44:16 +0300

    недоделанная функция расчета времени робочего дня и встречи

--- js/functions.js
+++ js/functions.js
@@ -53,3 +53,36 @@ controlStringLength('проверяемая строка', 20);
 // };
 
 // isPalindrome();
+
+const workingHours = (startWork, endWork, startMeeting, durationMeeting) => {
+  // начало рабочего дня
+  const startWorkData = startWork.split(':');
+  const startWorkHours = parseInt(startWorkData[0], 10);
+  const startWorkMinutes = parseInt(startWorkData[1], 10);
+  // конец рабочего дня
+  const endWorkData = endWork.split(':');
+  const endtWorkHours = parseInt(endWorkData[0], 10);
+  const endWorkMinutes = parseInt(endWorkData[1], 10);
+  // начало встречи
+  const startMeetingHours = parseInt(startMeeting[0], 10);
+  const startMeetingMinutes = parseInt(startMeeting[1], 10);
+
+  // конец встречи
+  let endMeetingHours = Math.floor(
+    (startMeetingHours * 60 + startMeetingMinutes + durationMeeting) / 60
+  );
+  let endMeetingMinutes = (durationMeeting + startMeetingMinutes) % 60;
+
+  if (startWork === startMeeting || endWork - startWork >= durationMeeting) {
+    return true;
+  }
+  return false;
+};
+
+console.log(
+  workingHours('08:00', '17:30', '14:00', 90), // true
+  workingHours('8:0', '10:0', '8:0', 120), // true
+  workingHours('08:00', '14:30', '14:00', 90), // false
+  workingHours('14:00', '17:30', '08:0', 90), // false
+  workingHours('8:00', '17:30', '08:00', 900) // false;
+);
