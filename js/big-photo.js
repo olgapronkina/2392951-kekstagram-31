53ab31cf539832a0d93737d97f936790772a107c
Olga Pronkina <pronolga04@gmail.com>
2024-05-20 18:13:53 +0300

    улучшает пользовательский интерфейс

--- js/big-photo.js
+++ js/big-photo.js
@@ -1,20 +1,19 @@
 import './render-photos';
 import { isEscapeKey } from './util.js';
+import { clearComments, renderComments } from './render-comments.js';
 
 const bigPictureElement = document.querySelector('.big-picture'); //шаблон для большого поста
 const bigPictureImgElement = document
   .querySelector('.big-picture__img')
   .querySelector('img'); //нода для отображения выбранной по клику фотографии
 const likesCountElement = bigPictureElement.querySelector('.likes-count'); //элемент с кол-вом лайков
-const socialCommentsNode = bigPictureElement.querySelector('.social__comments'); //нода со списком комментов
-const socialCommentTemplate =
-  socialCommentsNode.querySelector('.social__comment'); //шаблон коментариев
-const socialShownComments = bigPictureElement.querySelector(
-  '.social__comment-shown-count'
-);
-const socialTotalComments = bigPictureElement.querySelector(
-  '.social__comment-total-count'
-);
+
+// const socialShownComments = bigPictureElement.querySelector(
+//   '.social__comment-shown-count'
+// );
+// const socialTotalComments = bigPictureElement.querySelector(
+//   '.social__comment-total-count'
+// );
 
 const commentCaptionNode = bigPictureElement.querySelector('.social__caption'); //элемент с описанием фотографии
 const commentsCountNode = bigPictureElement.querySelector(
@@ -37,6 +36,7 @@ const onDocumentKeydown = (evt) => {
 };
 
 function closeBigPicture() {
+  clearComments();
   bigPictureElement.classList.add('hidden');
   bigPictureCancel.removeEventListener('click', onBigPictureCancelClick);
   document.removeEventListener('keydown', onDocumentKeydown);
@@ -44,29 +44,13 @@ function closeBigPicture() {
 
 const openBigPicture = (posts, pictureId) => {
   const currentPhoto = posts.find((photo) => photo.id === Number(pictureId));
-  const socialCommentsFragment = document.createDocumentFragment();
 
   bigPictureImgElement.src = currentPhoto.url; //присвоили большой картинке ссылку
   likesCountElement.textContent = currentPhoto.likes; //присвоили большой картинке лайки
-  socialCommentsNode.innerHTML = '';
-  socialShownComments.textContent = currentPhoto.comments.length;
-  socialTotalComments.textContent = currentPhoto.comments.length;
+  commentCaptionNode.textContent = currentPhoto.description;
 
   //комментарии
-  currentPhoto.comments.forEach((comment) => {
-    const socialCommentNode = socialCommentTemplate.cloneNode(true);
-
-    socialCommentNode.querySelector('.social__picture').src = comment.avatar;
-    socialCommentNode.querySelector('.social__picture').alt = comment.name;
-    socialCommentNode.querySelector('.social__text').textContent =
-      comment.message;
-    socialCommentsFragment.appendChild(socialCommentNode);
-  });
-
-  socialCommentsNode.appendChild(socialCommentsFragment);
-  commentCaptionNode.textContent = currentPhoto.description;
-  commentsCountNode.classList.add('hidden');
-  commentsLoaderNode.classList.add('hidden');
+  renderComments(currentPhoto.comments);
 
   bigPictureElement.classList.remove('hidden');
   bigPictureCancel.addEventListener('click', onBigPictureCancelClick);
---
+++ js/render-comments.js
@@ -0,0 +1,61 @@
+const COUNT_STEP = 5;
+let currentCount = 0;
+let comments = [];
+
+const bigPictureElement = document.querySelector('.big-picture'); //шаблон для большого поста
+const socialCommentsNode = bigPictureElement.querySelector('.social__comments'); //нода со списком комментов
+const socialCommentTemplate =
+  socialCommentsNode.querySelector('.social__comment'); //шаблон коментов
+const commentsCountNode = bigPictureElement.querySelector(
+  '.social__comment-count'
+);
+const commentsLoaderNode = bigPictureElement.querySelector('.comments-loader'); //Кнопка для загрузки новой порции комментов
+const socialShownComments = bigPictureElement.querySelector(
+  '.social__comment-shown-count'
+);
+const socialTotalComments = bigPictureElement.querySelector(
+  '.social__comment-total-count'
+);
+socialCommentsNode.innerHTML = '';
+
+const renderNextComments = () => {
+  const socialCommentsFragment = document.createDocumentFragment();
+  const renderedComments = comments.slice(
+    currentCount,
+    currentCount + COUNT_STEP
+  );
+  const renderedCommentsLength = renderedComments.length + currentCount;
+
+  renderedComments.forEach((comment) => {
+    const socialCommentNode = socialCommentTemplate.cloneNode(true);
+
+    socialCommentNode.querySelector('.social__picture').src = comment.avatar;
+    socialCommentNode.querySelector('.social__picture').alt = comment.name;
+    socialCommentNode.querySelector('.social__text').textContent =
+      comment.message;
+    socialCommentsFragment.appendChild(socialCommentNode);
+  });
+  socialCommentsNode.appendChild(socialCommentsFragment);
+  socialShownComments.textContent = renderedCommentsLength;
+  socialTotalComments.textContent = comments.length;
+
+  if (renderedCommentsLength >= comments.length) {
+    commentsLoaderNode.classList.add('hidden');
+  }
+  currentCount += COUNT_STEP;
+};
+
+const clearComments = () => {
+  currentCount = 0;
+  socialCommentsNode.innerHTML = '';
+  commentsLoaderNode.classList.remove('hidden');
+  commentsLoaderNode.removeEventListener('click', renderNextComments);
+};
+
+const renderComments = (currentPhotoComments) => {
+  comments = currentPhotoComments;
+  renderNextComments();
+  commentsLoaderNode.addEventListener('click', renderNextComments);
+};
+
+export { clearComments, renderComments };
